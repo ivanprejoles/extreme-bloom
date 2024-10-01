a2fc8cdabd2f52af97bc0974822c7b24135c90c6
ivanprejoles <ivanpatrickprejoles@gmail.com>
2024-10-01 17:05:46 +0800

    new

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,10 +1,13 @@
-import {PrismaClient} from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 declare global {
     var prisma: PrismaClient | undefined;
 }
 
-const client = globalThis.prisma || new PrismaClient()
+const client = globalThis.prisma || new PrismaClient();
+if (!globalThis.prisma) {
+    console.log('Creating new Prisma client instance');
+}
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = client;
 
 export default client;
