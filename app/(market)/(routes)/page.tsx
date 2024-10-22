6bb42c9183c5904363b8880966ee835784b17061
ivanprejoles <ivanpatrickprejoles@gmail.com>
2024-10-22 20:19:15 +0800

    new

--- app/(market)/(routes)/page.tsx
+++ app/(market)/(routes)/page.tsx
@@ -1,17 +1,25 @@
 import CategorySelection from "@/components/general/category-selection";
 import CategoryItems from "@/components/general/category-items";
-import {AppleCardsCarouselDemo} from "@/components/general/event-carousel";
+import { AppleCardsCarouselDemo } from "@/components/general/event-carousel";
 import { MapDrawer } from "@/components/general/map-drawer";
 import { getCategories } from "@/app/actions/get-categories";
 import Image from "next/image";
-import client from "@/lib/prismadb";
 import { getEvents } from "@/app/actions/get-events";
 
-export default async function Home() {
+export async function getStaticProps() {
+  const categories = await getCategories();
+  const events = await getEvents();
 
-  const categories = await getCategories()
-  const events = await getEvents()
-  
+  return {
+    props: {
+      categories,
+      events,
+    },
+    revalidate: 60, // Revalidate every 60 seconds
+  };
+}
+
+export default function Home({ categories, events }: any) {
   return (
     <div className="w-full h-auto min-h-[100vh] flex flex-col relative">
       <div className="h-[20rem] md:h-[40rem]">
@@ -25,8 +33,8 @@ export default async function Home() {
         </div>
       </div>
       <div className="bg-transparent h-auto min-h-[20rem] -mt-[6rem]"> 
-          <CategorySelection categories={categories} />
-          <AppleCardsCarouselDemo events={events}  />
+        <CategorySelection categories={categories} />
+        <AppleCardsCarouselDemo events={events} />
         <div className="w-full h-auto px-2 md:px-10" id="item-storage">
           <CategoryItems />
         </div>
