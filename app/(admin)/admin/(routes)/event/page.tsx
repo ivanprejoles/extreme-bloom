f16f4b9f0eaae55bab12ee110f66871de49f88d4
ivanprejoles <ivanpatrickprejoles@gmail.com>
2024-10-24 09:17:08 +0800

    no-cache

--- app/(admin)/admin/(routes)/event/page.tsx
+++ app/(admin)/admin/(routes)/event/page.tsx
@@ -1,17 +1,11 @@
-import React from 'react'
-import client from '@/lib/prismadb'
-import EventsTable from '../../_components/events-table'
+import client from '@/lib/prismadb';
+import EventsTable from '../../_components/events-table';
 import { unstable_noStore as noStore } from 'next/cache';
 
-
-export const getServerSideProps = async ({ res }: any) => {
+// This component is a server component by default in Next.js App Router
+const EventPage = async () => {
   noStore();
-
-  // Disable caching for this page on Vercel and the browser
-  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
-  res.setHeader('Pragma', 'no-cache');
-  res.setHeader('Expires', '0');
-
+  // Fetch events from Prisma
   const events = await client.event.findMany({
     select: {
       id: true,
@@ -21,14 +15,6 @@ export const getServerSideProps = async ({ res }: any) => {
     },
   });
 
-  return {
-    props: {
-      events,
-    },
-  };
-};
-
-const EventPage = ({ events }: any) => {
   return (
     <div className="flex min-h-screen w-full flex-col">
       <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
